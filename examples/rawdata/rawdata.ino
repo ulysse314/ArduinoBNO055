8be40e888e8e5b887a1735942a20a2b5c24e179a
moussaillon <moussaillon>
2020-08-06 22:55:02 +0200

    Updating rawdata example

--- examples/rawdata/rawdata.ino
+++ examples/rawdata/rawdata.ino
@@ -1,7 +1,5 @@
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
+#include <BNO055.h>
 
 /* This driver reads raw data from the BNO055
 
@@ -22,7 +20,7 @@
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+BNO055 bno;
 
 /**************************************************************************/
 /*
@@ -40,18 +38,24 @@ void setup(void)
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    while(1) {
+      Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+      delay(5000);
+    }
   }
 
   delay(1000);
 
   /* Display the current temperature */
-  int8_t temp = bno.getTemp();
-  Serial.print("Current Temperature: ");
-  Serial.print(temp);
-  Serial.println(" C");
-  Serial.println("");
+  int8_t temp;
+  if (bno.getTemperature(&temp)) {
+    Serial.print("Current Temperature: ");
+    Serial.print(temp);
+    Serial.println(" C");
+    Serial.println("");
+  } else {
+    Serial.println("Temperature failed");
+  }
 
   bno.setExtCrystalUse(true);
 
@@ -64,7 +68,7 @@ void setup(void)
     should go here)
 */
 /**************************************************************************/
-void loop(void)
+void loop()
 {
   // Possible vector values can be:
   // - VECTOR_ACCELEROMETER - m/s^2
@@ -73,42 +77,50 @@ void loop(void)
   // - VECTOR_EULER         - degrees
   // - VECTOR_LINEARACCEL   - m/s^2
   // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  imu::Vector<3> euler;
+  if (!bno.getVector(BNO055::VECTOR_EULER, &euler)) {
+    Serial.println("Failed to get euler vector");
+  } else {
+    /* Display the floating point data */
+    Serial.print("X: ");
+    Serial.print(euler.x());
+    Serial.print(" Y: ");
+    Serial.print(euler.y());
+    Serial.print(" Z: ");
+    Serial.print(euler.z());
+    Serial.print("\t\t");
+  }
 
-  /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(euler.x());
-  Serial.print(" Y: ");
-  Serial.print(euler.y());
-  Serial.print(" Z: ");
-  Serial.print(euler.z());
-  Serial.print("\t\t");
-
-  /*
   // Quaternion data
-  imu::Quaternion quat = bno.getQuat();
-  Serial.print("qW: ");
-  Serial.print(quat.w(), 4);
-  Serial.print(" qX: ");
-  Serial.print(quat.x(), 4);
-  Serial.print(" qY: ");
-  Serial.print(quat.y(), 4);
-  Serial.print(" qZ: ");
-  Serial.print(quat.z(), 4);
-  Serial.print("\t\t");
-  */
+  imu::Quaternion quat;
+  if (!bno.getQuat(&quat)) {
+    Serial.println("Failed to get quat");
+  } else {
+    Serial.print("qW: ");
+    Serial.print(quat.w(), 4);
+    Serial.print(" qX: ");
+    Serial.print(quat.x(), 4);
+    Serial.print(" qY: ");
+    Serial.print(quat.y(), 4);
+    Serial.print(" qZ: ");
+    Serial.print(quat.z(), 4);
+    Serial.print("\t\t");
+  }
 
   /* Display calibration status for each sensor. */
   uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.print("CALIBRATION: Sys=");
-  Serial.print(system, DEC);
-  Serial.print(" Gyro=");
-  Serial.print(gyro, DEC);
-  Serial.print(" Accel=");
-  Serial.print(accel, DEC);
-  Serial.print(" Mag=");
-  Serial.println(mag, DEC);
+  if (!bno.getCalibration(&system, &gyro, &accel, &mag)) {
+    Serial.println("Failed to get calibration");
+  } else {
+    Serial.print("CALIBRATION: Sys=");
+    Serial.print(system, DEC);
+    Serial.print(" Gyro=");
+    Serial.print(gyro, DEC);
+    Serial.print(" Accel=");
+    Serial.print(accel, DEC);
+    Serial.print(" Mag=");
+    Serial.println(mag, DEC);
+  }
 
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
